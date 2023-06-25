48962401ded5582f0d3631e88adf7bf0fbefc959
Ramanpreet Kaur <120923608+rkaur050@users.noreply.github.com>
2023-06-25 09:18:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function toggleHide(){
+    let btn=document.getElementById('menu')
+    let ele=document.getElementById('elements')
+    if(ele.style.display!='none')
+       ele.style.display='none'
+    else
+       ele.style.display='block'   
+}
